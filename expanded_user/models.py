4ce640a604e6e3256044a554f94a023e486b5236
goranaviani <goranaviani@gmail.com>
2019-09-08 16:37:48 +0200

    Expanded user model

--- expanded_user/models.py
+++ expanded_user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class custom_user(AbstractUser):
+    userMobileNumber = models.CharField(max_length=50,null=True, blank=True)
+    userAddress = models.CharField(max_length=30,null=True, blank=True)
+    userCity = models.CharField(max_length=30,null=True, blank=True)
+    userCountry = models.CharField(max_length=30,null=True, blank=True)
+    #userDoB = models.DateField(null=True, blank=True)
+    #userSecondEmail = models.EmailField(max_length=50,null=True, blank=True)
+
+
+    def __str__(self):
+        return self.email
+
+
+
+
--- incodaq_weather/settings.py
+++ incodaq_weather/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'expanded_user.apps.ExpandedUserConfig',
 ]
 
 MIDDLEWARE = [
