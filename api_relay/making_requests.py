96e4022b5be80881eedbbf2df39e655eb3eefc47
goranaviani <goranaviani@gmail.com>
2019-09-16 12:21:34 +0200

    Added - api relay making  API requests module and views

---
+++ api_relay/making_requests.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import twilio.rest
+#from twilio.rest import Client
+
+def make_request_params(**kwargs):
+    try:
+        apiUrl = kwargs["apiUrl"]
+    except:
+        return ({"testing_apis function": "The main url for the API is missing"})
+
+    try:
+        apiEndpoint = kwargs["apiEndpoint"]
+        # Add an endpoint to the api
+        fullAPIUrl = apiUrl + apiEndpoint
+    except:
+        return ({"testing_apis": "The API endpoint for te API is missing"})
+
+    try:
+        params1 = kwargs["params1"]
+        result = requests.get(fullAPIUrl, params=params1)
+    except:
+        return ({"testing_api": "Missing API data"})
+
+
+    if result.status_code in (400, 401, 402, 403, 404):
+        return result
+
+    return result.json()
+
+def twilio_api(userMobileNumber, processedForecastMessage, twilioAccountSid, twilioAuthToken, myTwilioTelephone):
+    
+    client1 = twilio.rest.Client(twilioAccountSid, twilioAuthToken)
+
+    message1 = client1.messages \
+                    .create(
+                        body=processedForecastMessage,
+                        from_= myTwilioTelephone,
+                        to=userMobileNumber
+                    )
+    #print(message1.status)
--- api_relay/views.py
+++ api_relay/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from incodaq_weather.local_settings import locationiqTokenKey, darkSkyToken, twilioAccountSid, twilioAuthToken, myTwilioTelephone
+
+
+# Create your views here.
+#This is the place where api functions are getting called from
+
+from .making_requests import make_request_params, twilio_api
+
+#returning users latitude and longitude
+def get_user_lat_long_api(stringToSend):
+    apiUrl = {"apiUrl": "https://eu1.locationiq.com/v1/"}
+    apiEndpoint = {"apiEndpoint": "search.php"}
+    params =  {"params1":{'key': locationiqTokenKey,
+        'q': stringToSend,
+        'format': 'json'}}
+    result = make_request_params(**apiUrl, **apiEndpoint, **params)
+    #get here lat and long
+    userLat = result[0]["lat"]
+    userLon = result[0]["lon"]
+    return userLat, userLon
+
+
+def get_user_weather_forecast_api(userLen, userLong):
+    
+    apiUrl = {"apiUrl": "https://api.darksky.net/forecast/"}
+    apiEndpoint = {"apiEndpoint": darkSkyToken + "/" + userLen +","+userLong}
+    params =  {"params1":{'units': "auto",}}
+    result = make_request_params(**apiUrl, **apiEndpoint, **params)
+    #print(result)    
+    return result
+
+def send_sms_message_api(userMobileNumber, processedForecastMessage):
+    twilio_api(userMobileNumber, processedForecastMessage, twilioAccountSid, twilioAuthToken, myTwilioTelephone)
