567d1b0413591016401a56462acce6ed957a63c5
goranaviani <goranaviani@gmail.com>
2019-09-17 16:45:25 +0200

    Sendning custom message to dashboard

--- incodaq_weather/views.py
+++ incodaq_weather/views.py
@@ -1,19 +1,35 @@
 #Location of non app single pages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-
+from mobile_phone.models import user_phone
 
 
 def index(request):
 #   return render(request,'index.html')
    if request.user.is_authenticated:
-      return render(request,'dashboard.html')
+      return redirect('dashboard')
    else:
       return render(request,'index.html')
 
 def dashboard(request):
    if request.user.is_authenticated:
-      return render(request, 'dashboard.html')
+      
+
+      dashboardStatus = "Hi, welcome to your dashboard."
+      statusColor = "green"
+  
+      
+      userMobileNumber = User.objects.filter(userMobilePhone=request.user)
+     # queryUserNote = note.objects.filter(noteUser=request.user).order_by('-noteTimestamp')
+      return render(request, 'dashboard.html', 
+      {
+      'dashboardStatus':dashboardStatus,
+      'statusColor': statusColor,
+      'userMobileNumber': userMobileNumber,
+     # 'queryUserNote': queryUserNote,
+      })
+
+
    else:
         #if user is not authenticated inform him of that
         return render(request, 'other_pages/not_authenticaded.html')
