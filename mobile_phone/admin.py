295d9f6b05d2c12f3d420c38e75a9020b5856ca5
goranaviani <goranaviani@gmail.com>
2019-09-17 15:32:53 +0200

    Added  user mobile to admin

--- mobile_phone/admin.py
+++ mobile_phone/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .models import user_phone
+
+
+class user_mobile_admin (admin.ModelAdmin):
+    list_display = ("phoneCountryCode" , "phoneNumber" , "isMobileValidated","sendWeatherSMS")
+    list_filter = ("phoneCountryCode", "phoneNumber" , "isMobileValidated","sendWeatherSMS")
+    search_fields= ("phoneCountryCode","phoneNumber", "isMobileValidated","sendWeatherSMS")
+
+
+admin.site.register(user_phone, user_mobile_admin)
