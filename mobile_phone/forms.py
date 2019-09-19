74cd8af34ede2ab0f28c781e8d174e28c3460ff0
goranaviani <goranaviani@gmail.com>
2019-09-19 17:05:57 +0200

    Quick fix phoneNumbe field from the mobile_phone model is now required=False

--- mobile_phone/forms.py
+++ mobile_phone/forms.py
@@ -6,7 +6,7 @@ from incodaq_weather.choice import FORECAST_HOURS, MOBILE_PHONE_COUNTY_CODE
 class user_mobile_phone_form(forms.ModelForm):
     #userMobilePhone = forms.CharField(label='', widget=forms.TextInput(attrs={'readonly':'readonly','class':'form-control'}))
     phoneCountryCode = forms.ChoiceField(required=False, widget=forms.Select, choices=MOBILE_PHONE_COUNTY_CODE,)
-    phoneNumber = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control'}))
+    phoneNumber = forms.CharField(required=False,label='', widget=forms.TextInput(attrs={'class':'form-control'}))
     wantsToReceiveWeatherSMS = forms.BooleanField(required=False)
     timeWeatherSMS = forms.ChoiceField(required=False, widget=forms.Select, choices=FORECAST_HOURS,)
     class Meta:
