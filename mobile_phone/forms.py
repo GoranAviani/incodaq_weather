2fd7da7fe04a17f21a276edb16bd5ed2fa2ff58a
goranaviani <goranaviani@gmail.com>
2019-09-19 17:03:02 +0200

    Added - phone country code is a choice field now featuring only Sweden and Croatia

--- mobile_phone/forms.py
+++ mobile_phone/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import user_phone
-from incodaq_weather.choice import FORECAST_HOURS
+from incodaq_weather.choice import FORECAST_HOURS, MOBILE_PHONE_COUNTY_CODE
 
 
 class user_mobile_phone_form(forms.ModelForm):
     #userMobilePhone = forms.CharField(label='', widget=forms.TextInput(attrs={'readonly':'readonly','class':'form-control'}))
-    phoneCountryCode = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control'}))
+    phoneCountryCode = forms.ChoiceField(required=False, widget=forms.Select, choices=MOBILE_PHONE_COUNTY_CODE,)
     phoneNumber = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control'}))
     wantsToReceiveWeatherSMS = forms.BooleanField(required=False)
     timeWeatherSMS = forms.ChoiceField(required=False, widget=forms.Select, choices=FORECAST_HOURS,)
