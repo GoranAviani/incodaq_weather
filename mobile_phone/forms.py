9be4164c697d635ec739183feda683577b060768
goranaviani <goranaviani@gmail.com>
2019-09-19 16:30:51 +0200

    Added more choices to the forecast hours. written comments and deleted not needed stuff

--- mobile_phone/forms.py
+++ mobile_phone/forms.py
@@ -8,14 +8,7 @@ class user_mobile_phone_form(forms.ModelForm):
     phoneCountryCode = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control'}))
     phoneNumber = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control'}))
     wantsToReceiveWeatherSMS = forms.BooleanField(required=False)
-    #timeWeatherSMS = forms.CharField(required=False, label='', widget=forms.TextInput(attrs={'class':'form-control'}))
-    
-    
-    timeWeatherSMS = forms.ChoiceField(
-        required=False,
-        widget=forms.Select,
-        choices=FORECAST_HOURS,
-    )
+    timeWeatherSMS = forms.ChoiceField(required=False, widget=forms.Select, choices=FORECAST_HOURS,)
     class Meta:
         model = user_phone
         fields = (
