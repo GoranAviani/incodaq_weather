8d3ad3b10f14bf1dc3147ccc91c0f26c37c31b2b
goranaviani <goranaviani@gmail.com>
2019-11-02 14:21:20 +0100

    writing tests for mobile validation

--- mobile_phone/tests.py
+++ mobile_phone/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .views import is_mobile_still_validated
+
+
+class MobileValidation(TestCase):
+    
+    def test_mobile_short(self):
+        result = is_mobile_still_validated("073123123", "073")
+        self.assertEqual(result, (False))
+
