540052048a31b7b08472aff104efd7f34395962b
goranaviani <goranaviani@gmail.com>
2019-11-10 19:49:08 +0100

    displaying default cities on admin

--- weather/admin.py
+++ weather/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import default_cities
+
+
+class default_cities_admin (admin.ModelAdmin):
+    list_display = ("Stockholm" , "Tokyo")
+    #list_filter = ("phoneCountryCode", "phoneNumber" , "isMobileValidated","wantsToReceiveWeatherSMS")
+    #search_fields= ("phoneCountryCode","phoneNumber", "isMobileValidated","wantsToReceiveWeatherSMS")
+
+
+admin.site.register(default_cities, default_cities_admin)
