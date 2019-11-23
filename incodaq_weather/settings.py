c9c136a3fa18c2863c23ca8ced6bf5940501742e
goranaviani <goranaviani@gmail.com>
2019-11-23 19:06:17 +0100

    Updating beat schedule

--- incodaq_weather/settings.py
+++ incodaq_weather/settings.py
@@ -157,8 +157,8 @@ from celery.schedules import crontab
 CELERY_BEAT_SCHEDULE = {
       'task-get_periodic_forecast_for_default_cities': {
        'task': 'weather.tasks.get_periodic_forecast_for_default_cities',
-        #'schedule': crontab(minute="*/30"),
-        'schedule': 30,
+        'schedule': crontab(minute="*/30"),
+        #'schedule': 30,
         
     },
 }
