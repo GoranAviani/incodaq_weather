2343e00a8025846c32b071a2a50de3f18fa7c068
goranaviani <goranaviani@gmail.com>
2019-12-04 19:46:02 +0100

    Updated requirements file

--- api_relay/views.py
+++ api_relay/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-#from incodaq_weather.local_settings import locationiqTokenKey, darkSkyToken, twilioAccountSid, twilioAuthToken, myTwilioTelephone
+from incodaq_weather.local_settings import locationiqTokenKey, darkSkyToken, twilioAccountSid, twilioAuthToken, myTwilioTelephone
 import os
 import logging
-locationiqTokenKey = os.environ.get("locationiqTokenKey", '')
-darkSkyToken = os.environ.get("darkSkyToken", '')
-twilioAccountSid = os.environ.get("twilioAccountSid", '')
-twilioAuthToken = os.environ.get("twilioAuthToken", '')
-myTwilioTelephone = os.environ.get("myTwilioTelephone", '')
+#locationiqTokenKey = os.environ.get("locationiqTokenKey", '')
+#darkSkyToken = os.environ.get("darkSkyToken", '')
+#twilioAccountSid = os.environ.get("twilioAccountSid", '')
+#twilioAuthToken = os.environ.get("twilioAuthToken", '')
+#myTwilioTelephone = os.environ.get("myTwilioTelephone", '')
 
 # Create your views here.
 #This is the place where api functions are getting called from
