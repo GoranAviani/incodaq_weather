54f1c0abe76425e413c2143c30c71f295bd6a2af
goranaviani <goranaviani@gmail.com>
2019-12-13 19:58:02 +0100

    Quickfix

--- weather/models.py
+++ weather/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
-    
+
 class default_cities(models.Model):
-    city = models.CharField(max_length=50,null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
     temperature = models.CharField(max_length=50, null=True, blank=True)
-   
+    weatherIconDesc = models.CharField(max_length=50, null=True, blank=True)
+    # weatherShortDescritpion = models.CharField(max_length=500,null=True, blank=True)
