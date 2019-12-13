7605add6aed6faaf449b1fddb8ad8fd8284ad2e3
goranaviani <goranaviani@gmail.com>
2019-12-13 19:56:54 +0100

    Quickfix

--- weather/views.py
+++ weather/views.py
@@ -6,5 +6,5 @@ from django.db import models
 class default_cities(models.Model):
     city = models.CharField(max_length=50, null=True, blank=True)
     temperature = models.CharField(max_length=50, null=True, blank=True)
-    weatherIcon = models.CharField(max_length=50, null=True, blank=True)
+    weatherIconDesc = models.CharField(max_length=50, null=True, blank=True)
     # weatherShortDescritpion = models.CharField(max_length=500,null=True, blank=True)
