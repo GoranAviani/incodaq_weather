8f36e06977af8e795a7a4efd01ad9b7a47f7b5de
goranaviani <goranaviani@gmail.com>
2019-12-13 19:46:35 +0100

    test

--- api_relay/making_requests.py
+++ api_relay/making_requests.py
@@ -41,4 +41,5 @@ def twilio_api(userMobileNumber, processedForecastMessage, twilioAccountSid, twi
                         from_= myTwilioTelephone,
                         to=userMobileNumber
                     )
-    #print(message1.status)
+    print(message1.status)
+    print(message1)
