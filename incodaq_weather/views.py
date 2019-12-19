ba2f205f593c79c566da2d6bd678386031182169
goranaviani <goranaviani@gmail.com>
2019-12-19 22:15:05 +0100

    Dashboard shows searchbar form field, dashboard view now loads search bar form field and on push method gets and prints out data

--- incodaq_weather/views.py
+++ incodaq_weather/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from .dashboard_status_processing import dashboard_status_processing
 from weather.tasks import get_periodic_forecast_for_default_cities
 from weather.models import default_cities
+from weather.forms import SearchBarForm
+
 
 def get_default_cities_temp():
    result = []
@@ -26,43 +28,51 @@ def index(request):
 
 def dashboard(request):
    if request.user.is_authenticated:
+      if request.method == 'POST':
+         form = SearchBarForm(request.POST)
+         if form.is_valid():
+            cd = form.cleaned_data
+            a = cd.get('searchBarInput')
+   #TODO
+      else:
+         user1 = {"user1": request.user}
 
-      user1 = {"user1": request.user}
-      
-      dashboardStatusMessage,dashboardStatusColor,hasMobileNumber, hasMobileNumberMessage, hasMobileNumberStatusColor, \
-      hasCityCountry, hasCityCountryMessage, hasCityCountryStatusColor, \
-      hasAddress,hasAddressMessage, hasAddressStatusColor, \
-      isMobileValidated, isMobileValidatedMessage, isMobileValidatedStatusColor, \
-      wantsToReceiveWeatherSMS, wantsToReceiveWeatherSMSMessage, wantsToReceiveWeatherSMSStatusColor, \
-      isForecastTimeSet, isForecastTimeSetMessage, isForecastTimeSetStatusColor \
-         = dashboard_status_processing(**user1)
-      
-    
-      return render(request, 'dashboard.html',
-      {
-         'dashboardStatus':dashboardStatusMessage,
-         'statusColor': dashboardStatusColor,
-         'hasMobileNumber': hasMobileNumber,
-         'hasMobileNumberMessage': hasMobileNumberMessage,
-         'hasMobileNumberStatusColor': hasMobileNumberStatusColor,
-         'hasCityCountry': hasCityCountry,
-         'hasCityCountryMessage': hasCityCountryMessage,
-         'hasCityCountryStatusColor': hasCityCountryStatusColor,
-         'hasAddress': hasAddress,
-         'hasAddressMessage': hasAddressMessage,
-         'hasAddressStatusColor': hasAddressStatusColor,
-         'isMobileValidated': isMobileValidated,
-         'isMobileValidatedMessage': isMobileValidatedMessage, 
-         'isMobileValidatedStatusColor': isMobileValidatedStatusColor,
-         
-         'wantsToReceiveWeatherSMS': wantsToReceiveWeatherSMS,
-         "wantsToReceiveWeatherSMSMessage": wantsToReceiveWeatherSMSMessage, 
-         "wantsToReceiveWeatherSMSStatusColor": wantsToReceiveWeatherSMSStatusColor,
+         dashboardStatusMessage,dashboardStatusColor,hasMobileNumber, hasMobileNumberMessage, hasMobileNumberStatusColor, \
+         hasCityCountry, hasCityCountryMessage, hasCityCountryStatusColor, \
+         hasAddress,hasAddressMessage, hasAddressStatusColor, \
+         isMobileValidated, isMobileValidatedMessage, isMobileValidatedStatusColor, \
+         wantsToReceiveWeatherSMS, wantsToReceiveWeatherSMSMessage, wantsToReceiveWeatherSMSStatusColor, \
+         isForecastTimeSet, isForecastTimeSetMessage, isForecastTimeSetStatusColor \
+            = dashboard_status_processing(**user1)
 
-         'isForecastTimeSet': isForecastTimeSet,
-         'isForecastTimeSetMessage': isForecastTimeSetMessage,
-         'isForecastTimeSetStatusColor': isForecastTimeSetStatusColor
-         })
+         searchBarInputForm = SearchBarForm()
+         return render(request, 'dashboard.html',
+         {
+            'dashboardStatus':dashboardStatusMessage,
+            'statusColor': dashboardStatusColor,
+            'hasMobileNumber': hasMobileNumber,
+            'hasMobileNumberMessage': hasMobileNumberMessage,
+            'hasMobileNumberStatusColor': hasMobileNumberStatusColor,
+            'hasCityCountry': hasCityCountry,
+            'hasCityCountryMessage': hasCityCountryMessage,
+            'hasCityCountryStatusColor': hasCityCountryStatusColor,
+            'hasAddress': hasAddress,
+            'hasAddressMessage': hasAddressMessage,
+            'hasAddressStatusColor': hasAddressStatusColor,
+            'isMobileValidated': isMobileValidated,
+            'isMobileValidatedMessage': isMobileValidatedMessage,
+            'isMobileValidatedStatusColor': isMobileValidatedStatusColor,
+
+            'wantsToReceiveWeatherSMS': wantsToReceiveWeatherSMS,
+            "wantsToReceiveWeatherSMSMessage": wantsToReceiveWeatherSMSMessage,
+            "wantsToReceiveWeatherSMSStatusColor": wantsToReceiveWeatherSMSStatusColor,
+
+            'isForecastTimeSet': isForecastTimeSet,
+            'isForecastTimeSetMessage': isForecastTimeSetMessage,
+            'isForecastTimeSetStatusColor': isForecastTimeSetStatusColor,
+            'searchBarInputForm': searchBarInputForm
+
+            })
 
 
    else:
