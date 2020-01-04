340dac84c898bd62bb0aa4211f10bd27464d16a0
goranaviani <goranaviani@gmail.com>
2020-01-04 18:01:19 +0100

    new make_request in making_requests.py module used to be used as data/params and get/post requests for the entire app

--- api_relay/making_requests.py
+++ api_relay/making_requests.py
@@ -4,6 +4,52 @@ import twilio.rest
 # import the logging library
 import logging
 
+def make_request(**kwargs):
+    try:
+        apiUrl = kwargs["apiUrl"]
+    except:
+        return "error"
+
+    try:
+        apiEndpoint = kwargs["apiEndpoint"]
+        # Add an endpoint to the api
+        fullAPIUrl = apiUrl + apiEndpoint
+    except:
+        return "error"
+
+    try:
+        paramsData = kwargs["paramsData"]
+        getPost = kwargs["getPost"]
+    except:
+        return "error"
+
+    if paramsData == "params" and getPost == "get":
+        try:
+            params1 = kwargs["params"]
+            result = requests.get(fullAPIUrl, params=params1)
+            logging.getLogger("darksky_info_logger").info("Dark Sky successful response: %s", result.json())
+        except:
+            logging.getLogger("error_logger").error("Api response is: %s", result.json())
+            return "error"
+
+    elif paramsData == "data" and getPost == "post":
+        try:
+            data1 = kwargs["data"]
+            #data1 = kwargs.get('data')
+            result = requests.post(fullAPIUrl, data=data1)
+            #TODO recaptcja logger
+            logging.getLogger("darksky_info_logger").info("recaptcha successful response: %s", result.json())
+        except:
+            logging.getLogger("error_logger").error("Recaptcha api response is: %s", result.json())
+            return "error"
+
+    if result.status_code in (400, 401, 402, 403, 404):
+        logging.getLogger("error_logger").error("40x Api response is: %s", result.json())
+        return result
+
+    return result.json()
+
+
 def make_request_params(**kwargs):
     try:
         apiUrl = kwargs["apiUrl"]
