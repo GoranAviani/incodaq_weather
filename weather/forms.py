187adc965dfb8775e2181aeaacecb1b3d874a027
goranaviani <goranaviani@gmail.com>
2020-01-04 15:53:50 +0100

    search box centered on the index.html

--- weather/forms.py
+++ weather/forms.py
@@ -3,4 +3,5 @@ from django.core.validators import RegexValidator
 
 class SearchBarForm(forms.Form): #Note that it is not inheriting from forms.ModelForm
     search_bar_validator = RegexValidator(r'^[\w, ]+$', "This value may contain only letters, numbers, comma or space.")
-    searchBarInput = forms.CharField(max_length=100, validators= [search_bar_validator], widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
+    #searchBarInput = forms.CharField(max_length=100, validators= [search_bar_validator], widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
+    searchBarInput = forms.CharField(max_length=100, validators=[search_bar_validator])
