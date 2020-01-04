46ad6ba36eb71ad74670aca11d37f6552b1485e8
goranaviani <goranaviani@gmail.com>
2020-01-04 16:15:44 +0100

    search box placeholder

--- weather/forms.py
+++ weather/forms.py
@@ -4,4 +4,5 @@ from django.core.validators import RegexValidator
 class SearchBarForm(forms.Form): #Note that it is not inheriting from forms.ModelForm
     search_bar_validator = RegexValidator(r'^[\w, ]+$', "This value may contain only letters, numbers, comma or space.")
     #searchBarInput = forms.CharField(max_length=100, validators= [search_bar_validator], widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
-    searchBarInput = forms.CharField(max_length=100, validators=[search_bar_validator])
+    searchBarInput = forms.CharField(help_text='This value may contain only letters, numbers, comma or space.' ,max_length=100, validators=[search_bar_validator],
+                    widget=forms.TextInput(attrs={'placeholder': 'Show me the weater in... city, country or place..'}))
